965a99d3cc48dfebccaffa1c225237d24fde892d
vickie1996 <37791552+vickie1996@users.noreply.github.com>
2021-05-08 19:56:06 +0530

    Create test_google_test.py
    Using pytest to execute the test cases and having setup and teardown module as well present in the code

---
+++ test_google_test.py
@@ -0,0 +1,21 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = None
+
+def setup_module(module):
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.implicitly_wait(5)
+    driver.delete_all_cookies()
+    driver.get("https://www.google.com")
+
+def teardown_module(module):
+    driver.quit()
+
+def test_google_title():
+    assert driver.title == "Google"
+
+def test_google_ur():
+    assert driver.current_url == 'https://www.google.com/'
