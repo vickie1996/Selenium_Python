fac60486e6f75c5eb7cc0098e4acb8eddbf99163
vickie1996 <37791552+vickie1996@users.noreply.github.com>
2021-05-10 13:44:16 +0530

    Update ConfigFixture.py

--- ConfigFixture.py
+++ ConfigFixture.py
@@ -16,3 +16,32 @@ def init_drivers(request):
 
         yield
         web_driver.close()
+
+        
+        
+        #another file which is using above file to get the Chrome and firefox launed using Fixture parameterizing
+        
+        import pytest
+from selenium.webdriver.common.by import By
+
+@pytest.mark.usefixtures('init_drivers')
+class BaseTest:
+    pass
+
+class Test_FB(BaseTest):
+
+    @pytest.mark.parametrize(
+                                "username, password",
+                                [
+                                    ('vivekberwal','admin123'),
+                                    ('vivek', 'admins1254')
+                                ]
+                            )
+
+    def test_login(self,username, password):
+        self.driver.get("https://www.facebook.com/")
+        self.driver.find_element(By.ID, 'email').send_keys(username)
+        self.driver.find_element(By.ID, 'pass').send_keys(password)
+
+
+
